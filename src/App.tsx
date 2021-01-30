344ec62bbb2ad1b5f070ed18219b010a18a39a52
Pablo Poggio <pablopoggiog@gmail.com>
2021-01-30 10:03:38 -0300

    Set up Firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+// import { useCollectionData } from "react-firebase-hooks/firestore";
+import { Chat, Login } from "src/containers";
 
-function App() {
-  return <div></div>;
-}
+firebase.initializeApp({
+  apiKey: "AIzaSyBQLgUQPLwLJXFI-aLv9nZqSxMNvMbYILI",
+  authDomain: "react-ts-talk.firebaseapp.com",
+  projectId: "react-ts-talk",
+  storageBucket: "react-ts-talk.appspot.com",
+  messagingSenderId: "922795508972",
+  appId: "1:922795508972:web:97b80396f1dae5b26bd1c9",
+  measurementId: "G-YPZSXBZJ3Y",
+});
+
+const auth = firebase.auth();
+// const firestore = firebase.firestore();
+
+const App = () => {
+  const [user] = useAuthState(auth);
+
+  return (
+    <div>
+      <section>{user ? <Chat /> : <Login />}</section>
+    </div>
+  );
+};
 
 export default App;
