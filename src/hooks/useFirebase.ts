72deae48045ac685a30518def8c28b5391bfe25c
Pablo Poggio <pablopoggiog@gmail.com>
2021-01-31 02:34:50 -0300

    Add build:dev script

--- src/hooks/useFirebase.ts
+++ src/hooks/useFirebase.ts
@@ -11,7 +11,7 @@ firebase.initializeApp({
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASURAMENT_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 });
 
 const auth = firebase.auth();
