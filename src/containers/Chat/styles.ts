357f929bffce7d0cab686479670ce42c2005e93d
Pablo Poggio <pablopoggiog@gmail.com>
2021-01-31 12:43:57 -0300

    Fix Chat input styles

--- src/containers/Chat/styles.ts
+++ src/containers/Chat/styles.ts
@@ -32,6 +32,12 @@ export const Input = styled.input`
   height: 35px;
   border-radius: 0.4em;
   border: solid 2.8px grey;
+  padding: 0.1em 1em 0;
+  width: 60%;
+
+  &:focus {
+    outline: none;
+  }
 `;
 
 export const SubmitButton = styled(Button)``;
--- src/containers/Logout/styles.ts
+++ src/containers/Logout/styles.ts
@@ -6,4 +6,9 @@ export const Container = styled.div<ContainerProps>`
   display: flex;
   justify-items: flex-start;
   width: 100%;
+
+  @media all and (min-width: 500px) {
+    width: 100px;
+    margin-right: auto;
+  }
 `;
