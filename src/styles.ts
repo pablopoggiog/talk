0bfefccbf478441df6f12cb5380772805e44b31c
Pablo Poggio <pablopoggiog@gmail.com>
2021-01-31 02:59:56 -0300

    Fix vertical scrolling for mobile devices b/c of the browser top bar

--- src/styles.ts
+++ src/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  min-height: 100vh;
+  min-height: 75vh;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
