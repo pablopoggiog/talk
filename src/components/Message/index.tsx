dd03c8c1b0610b2656c849a3ad804b848730ebf3
Pablo Poggio <pablopoggiog@gmail.com>
2021-02-02 01:06:07 -0300

    Remove unused imports from Message

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState, useEffect } from "react";
+import React, { FunctionComponent } from "react";
 import { Container, Photo, Text, DateFooter, Content } from "./styles";
 import { useFirebase, useDate } from "src/hooks";
 
