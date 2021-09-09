c23bec6c507a6079a5b742ca1df1db505d7d6abc
Pablo Poggio <pablopoggiog@gmail.com>
2021-09-08 23:12:51 -0300

    Use loading to show the spinner or the proper screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import { useFirebase } from "src/hooks";
+import { Spinner } from "src/components";
 import { Chat, Login, Logout } from "src/containers";
 import { Container } from "./styles";
 
 const App: FunctionComponent = () => {
-  const { user } = useFirebase();
+  const { user, loading } = useFirebase();
 
   return (
     <Container>
-      {user ? (
+      {loading ? (
+        <Spinner />
+      ) : user ? (
         <>
           <Logout />
           <Chat />
