c8df7d3afcfd366734006234ac172dd4bfa51096
Pablo Poggio <pablopoggiog@gmail.com>
2021-09-09 22:09:22 -0300

    Add test for the execution of sendMessage when the user tries to send a message

--- src/containers/Chat/__tests__/Chat.test.tsx
+++ src/containers/Chat/__tests__/Chat.test.tsx
@@ -1,4 +1,5 @@
 import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { MessageInterface } from "src/types";
 import { Chat } from "..";
 
@@ -57,5 +58,18 @@ describe("Chat", () => {
     );
 
     messages.forEach((message) => expect(message).toBeInTheDocument());
+  });
+
+  it("calls sendMessage the correct number of times and with the right parameters", () => {
+    render(<Chat />);
+    const newMessageText = "heeey!";
+
+    const input = screen.getByRole("textbox");
+
+    userEvent.type(input, newMessageText);
+    userEvent.type(input, "{enter}");
+
+    expect(mockedSendMessage).toHaveBeenCalledTimes(1);
+    expect(mockedSendMessage).toHaveBeenCalledWith(newMessageText);
   });
 });
